a6dad3df6ce04d5c3aa10d5e736cf31832432690
Alifseen <alifseen@gmail.com>
2024-10-22 13:56:37 +0500

    We set up variables and conditional to track new object and trigger an action once the object leaves.

--- Section 36.py
+++ Section 36.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy
 import time
+from emailer import send_email
 
 # ## Loading an Image
 # image1 = cv2.imread("files/image.png")
@@ -33,8 +34,12 @@ time.sleep(1)
 
 ## 1. Store first frame
 FIRST_FRAME = None
+live_object_status = []
 
 while True:
+    ## 15. Set up empty variables to use to trigger an action
+    object_status = 0
+
     ## save frame from video
     check, frame = video.read()
 
@@ -69,7 +74,21 @@ while True:
         ## 12. if the new outline is strong, store its values and create a rectangle around it
         x, y, w, h = cv2.boundingRect(contour)
         ## we add the frame where it will be created, starting points, ending points, color, and number of colors (in case of BGR that is 3)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
+        object_rect = cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
+
+        ## 13. Now we trigger an action whenever a new object comes in. The goal is to trigger an action once the object leaves the frames.
+        if object_rect.any():
+            ## 14. If there is a rectangle that means there is a new object, in which case, we will change the status to 1, as long as there is an object, this will be 1.
+            object_status = 1
+
+    ## 16. We use the list of status to track and initiate the object. We only need the last two values since we are looking for 1,0 which means there was an object (1) but now there is not (0)
+    live_object_status.append(object_status)
+    live_object_status = live_object_status[-2:]
+
+    ## 17. We trigger an action based on the condition that the object has left.
+    if live_object_status[0] == 1 and live_object_status[1] == 0:
+        ## 18. Send email
+        send_email()
 
     ## Display video
     cv2.imshow("1st Video", frame)
